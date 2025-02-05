7f8628881cb2ceeec54d89a06fc407fd20a2ba71
Filip Lindström <Filip.lindstrom@elev.ntig.ga.se>
2025-02-05 12:49:56 +0100

    skapat fömster programm

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace prog1_wpf_TE23E_Lindstrom_Filip;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
---
+++ EventApp/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace EventApp;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class MainWindow : Application
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void KlickaOk(Object sender, RoutedEventArgs e)
+    {
+        //LÄs av textrutan
+        string text = tbxText.Text;
+        MessageBox.Show("Du klickade Ok");
+    }
+
+    private void KlickaAvbryt(Object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show("Du klickade Avbryt");
+    }
+
+
+}
+
