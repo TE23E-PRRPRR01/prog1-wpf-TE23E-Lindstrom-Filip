83958e1858d6870bc4e78d0cbc58c66f9621c776
Filip Lindström <Filip.lindstrom@elev.ntig.ga.se>
2025-05-05 14:55:19 +0200

    Börjat med koden

---
+++ Ceasarkryptering/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32.SafeHandles;
+
+namespace Ceasarkryptering;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void KlickKryptera(object sender, RoutedEventArgs e)
+    {
+
+        //Läs av 
+        string inputText = txbInput.Text;
+        string keyText = txbKey.Text;
+
+        // konvertera keytext till heltal (int)
+        int.TryParse(keyText, out int key);
+
+        //rensa txbOutput
+        txbOutput.Text = "";
+
+        //Gå igenom det man skrivit
+        //loopa för bokstav
+        foreach (var bokstav in inputText)
+        {
+            // hitta ascii värde
+            int ascii = (int)bokstav;
+
+            // Kryptera dvs skifta framåt
+            int asciikryp = ascii + key;
+            char bokstavkryp = (char)asciikryp;
+
+            txbOutput.Text += $"{bokstavkryp}";
+        }
+    }
+}
---
+++ KalkApp/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KalkApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void KlickPlus(object sender, RoutedEventArgs e)
+    {
+        // Läsa av textrutorna
+        string tal1 = txbTal1.Text;
+        string tal2 = txbTal2.Text;
+
+        //Omvandla till heltal
+        Double.TryParse(tal1, out Double talint1);
+        Double.TryParse(tal2, out Double talint2);
+
+        // Uträkning
+        Double resultat = talint1 + talint2;
+
+        //Skriv ut resultatrutan
+        txbResult.Text = $"{resultat}";
+
+    }
+
+    private void KlickMinus(object sender, RoutedEventArgs e)
+    {
+        // Läsa av textrutorna
+        string tal1 = txbTal1.Text;
+        string tal2 = txbTal2.Text;
+
+        //Omvandla till heltal
+        Double.TryParse(tal1, out Double talint1);
+        Double.TryParse(tal2, out Double talint2);
+
+        // Uträkning
+        Double resultat = talint1 - talint2;
+
+        //Skriv ut resultatrutan
+        txbResult.Text = $"{resultat}";
+
+    }
+
+    private void KlickGånger(object sender, RoutedEventArgs e)
+    {
+        // Läsa av textrutorna
+        string tal1 = txbTal1.Text;
+        string tal2 = txbTal2.Text;
+
+        //Omvandla till heltal
+        Double.TryParse(tal1, out Double talint1);
+        Double.TryParse(tal2, out Double talint2);
+
+        // Uträkning
+        Double resultat = talint1 * talint2;
+
+        //Skriv ut resultatrutan
+        txbResult.Text = $"{resultat}";
+
+    }
+
+    private void KlickDelat(object sender, RoutedEventArgs e)
+    {
+        // Läsa av textrutorna
+        string tal1 = txbTal1.Text;
+        string tal2 = txbTal2.Text;
+
+        //Omvandla till heltal
+        Double.TryParse(tal1, out Double talint1);
+        Double.TryParse(tal2, out Double talint2);
+
+        // Uträkning
+        Double resultat = talint1 / talint2;
+
+        //Skriv ut resultatrutan
+        txbResult.Text = $"{tal1}/{tal2}={resultat}";
+
+    }
+
+}
